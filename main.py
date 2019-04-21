ecf7dcb3d336cc4d94e746cda5cdfb040373f8fd
Haalon <Haalon@yandex.com>
2019-04-22 00:22:19 +0300

    Polish GUI classes further

--- main.py
+++ main.py
@@ -10,51 +10,53 @@ class App(Frame):
 		self.master['padx'] = 10
 		self.master['pady'] = 10
 		self.grid(sticky=N+E+S+W)
-		self.create()
-		utils.grid_weight_configure(self, col_val = [1,0])
+
+		self.create()		
 		self.mainloop()
 
 	def create(self):
-		self.field = Field(self)
-		self.field.grid(row = 0, column = 0, rowspan = 2, sticky=N+E+S+W)
+		self.GameCanvas = GameCanvas(self, bg = 'gray90')
+		self.GameCanvas.grid(row = 0, column = 0, rowspan = 2, sticky=N+E+S+W, pady=3)
 
 		self.stats = StatFrame(self)
-		self.stats.grid(row = 0, column = 1, sticky=N+E+S+W)
+		self.stats.grid(row = 0, column = 1, sticky=N+E+S+W, pady=3)
 		
 		self.controls = ControlFrame(self)
-		self.controls.grid(row = 1, column = 1, sticky=N+E+S+W)		
+		self.controls.grid(row = 1, column = 1, sticky=N+E+S+W, pady=3)		
+		utils.grid_weight_configure(self, col_val = [1,0])
 		
 
-class Field(Canvas):
-	def __init__(self, master=None, *ap, foreground="black", **an):
-		Canvas.__init__(self, master, *ap, **an)
+class GameCanvas(Canvas):
+	def __init__(self, *ap,  **an):
+		super().__init__( *ap, **an)
 
 
 class StatFrame(Frame):
-	def __init__(self, master=None):
-		super().__init__(master)
+	def __init__(self, *ap,  **an):
+		super().__init__(*ap,  **an)
 		self['bg'] = 'gray70'
 		self.create()
-		utils.grid_weight_configure(self)
+		
 
 	def create(self):
 		self.statLabel = Label(self, text='Stats', bg='gray70')
 		self.statLabel.grid(sticky=N)
+		utils.grid_weight_configure(self)
 
 		
 class ControlFrame(Frame):
-	def __init__(self, master=None):
-		super().__init__(master)
+	def __init__(self,  *ap,  **an):
+		super().__init__( *ap,  **an)
 		self['bg'] = 'gray70'
-		self.create()
-		utils.grid_weight_configure(self, row_val = 0, col_val = 0)
+		self.create()	
 
 
 	def create(self):
 		self.Quit = Button(self, text="Quit", highlightthickness=0, command=self.quit)
-		self.Quit.grid(sticky=S+W+E, padx=5, pady=3)
+		self.Quit.grid(row = 0, column = 0, sticky=S+W+E, padx=5, pady=3)
+
 		self.NewGame = Button(self, text="New Game", highlightthickness=0, command=self.quit)
-		self.NewGame.grid(sticky=S+W+E, padx=5, pady=3)
-
-
+		self.NewGame.grid(row = 1, column = 0, sticky=S+W+E, padx=5, pady=3)
+		utils.grid_weight_configure(self, row_val = [1,0], col_val = 1)
+		
 App()
